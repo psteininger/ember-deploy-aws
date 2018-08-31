addb3b8efe8d094296a94ad2f30b0fad1f82054b
Piotr Steininger <piotr.steininger@gmail.com>
2018-08-31 17:26:59 +0200

    Added S3 Bucket Policy

--- hosting/main.tf
+++ hosting/main.tf
@@ -33,3 +33,8 @@ resource "aws_s3_bucket" "frontend" {
     Environment = "${terraform.workspace}"
   }
 }
+resource "aws_s3_bucket_policy" "frontend" {
+  bucket = "${aws_s3_bucket.frontend.id}"
+  policy = "${data.aws_iam_policy_document.bucket_policy.json}"
+}
+
