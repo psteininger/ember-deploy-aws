ccfd165887a70124018f198675a67ab74f71892a
Piotr Steininger <piotr.steininger@gmail.com>
2018-08-31 16:42:40 +0200

    Added S3 bucket

---
+++ hosting/main.tf
@@ -0,0 +1,35 @@
+resource "aws_s3_bucket" "frontend" {
+  bucket = "${local.bucket_name}"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+  }
+
+  cors_rule {
+    allowed_headers = [
+      "*",
+    ]
+
+    allowed_methods = [
+      "GET",
+      "HEAD",
+    ]
+
+    allowed_origins = [
+      "http://${local.bucket_name}",
+      "https://${local.bucket_name}",
+    ]
+
+    expose_headers = [
+      "ETag",
+    ]
+
+    max_age_seconds = 3000
+  }
+
+  tags {
+    Name        = "${var.name}"
+    Environment = "${terraform.workspace}"
+  }
+}
